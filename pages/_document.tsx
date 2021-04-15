a3c963e2a20a4b27ec556b5f1a435b8a3ef493fc
ben39053372 <ben39053372@gmail.com>
2021-04-15 21:32:52 +0800

    - init project - upgrade dependencies - change to custom _document.tsx - integrate typescript

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+// @generated: @expo/next-adapter@2.1.5
+// export { default } from '@expo/next-adapter/document'
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
