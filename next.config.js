2528c9f8701d3ae001ed526cc586498e3fc028e6
Ben Cheng <bencheng@skylight-group.com>
2021-04-16 17:42:59 +0800

    step up expo-next-react-navigation

--- next.config.js
+++ next.config.js
@@ -2,10 +2,23 @@
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/guides/using-nextjs.md
 
 const { withExpo } = require('@expo/next-adapter')
+const withFonts = require('next-fonts')
+const withImages = require('next-images')
+const withTM = require('next-transpile-modules')
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-  // future: {
-  //   webpack5: true,
-  // },
-})
+module.exports = withPlugins(
+  [
+    [
+      withTM,
+      {
+        transpileModules: ['expo-next-react-navigation'],
+      },
+    ],
+    withFonts,
+    withImages,
+    [withExpo, { projectRoot: __dirname }],
+  ],
+  {
+  }
+)
