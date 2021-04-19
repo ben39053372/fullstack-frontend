a11b3fb6de965afaaa202dea5a0a0f69dd9832ad
Ben Cheng <bencheng@skylight-group.com>
2021-04-19 17:06:40 +0800

    add 404 and 500 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import { View, Text } from "react-native";
+
+export default function Custom404() {
+  return <View><Text>404 - Page Not Found</Text></View>
+}
---
+++ src/pages/500.tsx
@@ -0,0 +1,5 @@
+import { View, Text } from "react-native";
+
+export default function Custom500() {
+  return <View><Text>500 - Server-side error occurred</Text></View>
+}
