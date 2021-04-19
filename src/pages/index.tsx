d54dc7e50b720a41174d49883a5e7cbd7fec7d63
Ben Cheng <bencheng@skylight-group.com>
2021-04-19 18:05:38 +0800

    integrate i18n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 // @generated: @expo/next-adapter@2.1.5
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
+import '../i18n'
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const { t } = useTranslation();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> hi Welcome to Expo + Next.js 👋</Text>
+      <Text style={styles.text}> hi Welcome to Expo + Next.js {t('test')}👋</Text>
     </View>
   )
 }
