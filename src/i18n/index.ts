1bf801eefc3072c83c4a0ccba3d6ea19ded2a5d1
Ben Cheng <bencheng@skylight-group.com>
2021-04-21 10:05:57 +0800

    add config for i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,28 +1,30 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import en from './en.json'
-import zh_hk from './zh-HK.json'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import zh_hk from './zh-HK.json';
 
 const resources = {
   en: {
-    translation: en
+    translation: en,
   },
-  "zh-HK": {
-    translation: zh_hk
-  }
+  'zh-HK': {
+    translation: zh_hk,
+  },
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    ns: [],
     resources,
-    lng: "zh-HK",
-
+    lng: 'zh-HK',
+    debug: true,
     keySeparator: false, // we do not use keys in form messages.welcome
-
+    fallbackLng: 'zh-HK',
+    cleanCode: true,
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
-  export default i18n;
+export default i18n;
