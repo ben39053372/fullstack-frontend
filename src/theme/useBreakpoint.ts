399e2c23aba416e1228a13ed84c9caf390e0162d
Ben Cheng <bencheng@skylight-group.com>
2021-04-21 14:34:52 +0800

    add Hook useBreakpont

---
+++ src/theme/useBreakpoint.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { useWindowDimensions } from 'react-native';
+import { useCustomTheme } from './useCustomTheme';
+
+export const useBreakpoint = () => {
+  const { theme } = useCustomTheme();
+  const { width } = useWindowDimensions();
+  const [bp, setBp] = useState<string>('lg');
+
+  useEffect(() => {
+    const sortedBp = Object.entries(theme.Breakpoints)
+      .sort((a, b) => b[1] - a[1])
+      .find((bp) => bp[1] < width)[0];
+
+    setBp(sortedBp || theme.Breakpoints[Object.keys(theme.Breakpoints).length]);
+  }, [width]);
+
+  return bp;
+};
