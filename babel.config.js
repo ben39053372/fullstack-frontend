6f3e28cd8a3ab040e3cc1f16d02bb9034550530b
Ben Cheng <bencheng@skylight-group.com>
2021-04-21 16:26:42 +0800

    fix babel error

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 // @generated: @expo/next-adapter@2.1.5
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
 
-module.exports = { presets: ['@expo/next-adapter/babel'], plugins: ["@babel/plugin-proposal-class-properties"] }
+module.exports = {
+  presets: ['@expo/next-adapter/babel'],
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    ['@babel/plugin-proposal-class-properties'],
+  ],
+};
