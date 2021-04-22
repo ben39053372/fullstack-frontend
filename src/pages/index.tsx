c7a9ac8cfe561dbbd15b905e937c315c908988d9
Ben Cheng <bencheng@skylight-group.com>
2021-04-22 10:11:40 +0800

    remove react-native-elements component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 // @generated: @expo/next-adapter@2.1.5
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import '../i18n';
 import { useTranslation } from 'react-i18next';
 import { useAppDispatch } from '../hooks/redux/useAppDispatch';
 import { useAppSelector } from '../hooks/redux/useAppSelector';
 import { increment, decrement } from '../features/counter/counterSlice';
-import { Divider, Button } from 'react-native-elements';
 import { Link, useRouting } from 'expo-next-react-navigation';
 import Head from 'next/head';
 
@@ -23,48 +22,30 @@ const App = () => {
         <title>Home</title>
       </Head>
       <Text>hi Welcome to Expo + Next.js 👋</Text>
-      <Divider />
       <Text>i18n</Text>
-      {t('test')}
+      <Text>{t('test')}</Text>
       <View style={styles.section}>
         <Button
-          style={{ flex: 1 }}
           title="change to en"
           onPress={() => i18n.changeLanguage('en')}
         />
         <Button
-          style={{ flex: 1 }}
           title="change to zh-HK"
           onPress={() => i18n.changeLanguage('zh-hk')}
         />
       </View>
-      <Divider />
       <Text>Redux</Text>
 
       <Text>Count: {count}</Text>
       <View style={styles.section}>
-        <Button
-          style={{ flex: 1 }}
-          title="increment"
-          onPress={() => dispatch(increment())}
-        />
-        <Button
-          style={{ flex: 1 }}
-          title="decrement"
-          onPress={() => dispatch(decrement())}
-        />
+        <Button title="increment" onPress={() => dispatch(increment())} />
+        <Button title="decrement" onPress={() => dispatch(decrement())} />
       </View>
-      <Divider />
       <Text>React Navigation</Text>
       <View style={styles.section}>
         <Button
-          style={{ flex: 1 }}
           title="Goto About(useRouting)"
           onPress={() => navigate({ routeName: 'About' })}
-        />
-        <Button
-          style={{ flex: 1 }}
-          title={<Link routeName="About">{'Goto About(<Link />)'}</Link>}
         />
       </View>
     </View>
