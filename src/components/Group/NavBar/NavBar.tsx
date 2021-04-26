e5446d364e635c9b778dca169fb1721b2c4cf7f8
ben39053372 <ben39053372@gmail.com>
2021-04-26 22:39:18 +0800

    clear code

--- src/components/Group/NavBar/NavBar.tsx
+++ src/components/Group/NavBar/NavBar.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
+import { StyleSheet } from 'react-native';
 import { Container } from './NavBar.style';
 import { H2 } from '../../Atoms/Typography';
 
 export function NavBar() {
   return (
     <Container>
-      <Fontisto name="nav-icon" style={styles.icon} />
       <H2>NavBar</H2>
     </Container>
   );
