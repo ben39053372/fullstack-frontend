1ec9a4e67544edcf11d53c18bb5fac26f237eab5
Ben Cheng <bencheng@skylight-group.com>
2021-05-17 18:02:18 +0800

    update

--- src/components/Atoms/Redirect/Redirect.tsx
+++ src/components/Atoms/Redirect/Redirect.tsx
@@ -1,9 +1,18 @@
-import { useEffect } from 'react';
-import { useRouting } from 'expo-next-react-navigation';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { toLogin } from '../../../const/routes';
+import { Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export const Redirect = () => {
-  const { navigate } = useRouting();
-  useEffect(() => navigate(toLogin), []);
+  const router = useRouter();
+  const navigation = useNavigation();
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      router.replace('/');
+    } else {
+      navigation.navigate('/');
+    }
+  }, []);
   return <></>;
 };
