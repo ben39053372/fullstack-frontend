9a23b48b415961f5ad47337663bbe150efed4b02
Ben Cheng <bencheng@skylight-group.com>
2021-05-17 18:06:31 +0800

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useRouting } from 'expo-next-react-navigation';
 import React, { useContext, useState } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
@@ -50,40 +49,6 @@ export default function Login() {
       />
       <Button text="SIGN UP" textVariant="h3" onPress={() => {}} />
     </View>
-=======
-import Head from 'next/head';
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { View } from 'react-native';
-import { Text } from '../components/Atoms/Text';
-import { TextInput } from '../components/Atoms/TextInput'
-import { Button } from '../components/Groups/Button';
-
-export default function Login() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-
-  return (
-    <>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <View style={styles.container} >
-        <Text textVariant="h4" >Login Page</Text>
-        <Text>Username:</Text>
-        <TextInput value={username} onChangeText={setUsername} />
-        <View style={{ height: 10 }} />
-        <Text>Password:</Text>
-        <TextInput value={password} onChangeText={setPassword} />
-        <View style={{ height: 10 }} />
-        <View style={styles.rowView}>
-          <Button text="login" textVariant="h5" onPress={() => console.log("login")} />
-          <View style={{ width: 20 }} />
-          <Button text="sign up" textVariant="h5" onPress={() => console.log("sign up")} />
-        </View>
-      </View>
-    </>
->>>>>>> 829f9c27c81fdab5072a414a4b60ffab5412e8ec
   );
 }
 
